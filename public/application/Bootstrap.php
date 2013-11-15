cd3c9f2c9646d700b44df56fd5735189c6fb73bf
Ricardo Buquet <ricardo.buquet@groupon.com>
2013-11-15 15:49:59 -0300

    agrego rutas personalizadas

--- public/application/Bootstrap.php
+++ public/application/Bootstrap.php
@@ -3,6 +3,62 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-
+    /**
+     * Configures Zend Router settings.
+     * @return Zend_Controller_Router_Interface
+     */
+    protected function _initRouter()
+    {
+        $front = Zend_Controller_Front::getInstance();
+        $router = $front->getRouter();
+        $routes = array(
+            // Index Controller
+            'news' => new Zend_Controller_Router_Route(
+                'news',
+                array(
+                    'controller' => 'index',
+                    'action' => 'news'
+                )
+            ),
+            'tour' => new Zend_Controller_Router_Route(
+                'tour',
+                array(
+                    'controller' => 'index',
+                    'action' => 'tour'
+                )
+            ),
+            'gallery' => new Zend_Controller_Router_Route(
+                'gallery',
+                array(
+                    'controller' => 'index',
+                    'action' => 'gallery'
+                )
+            ),
+            'discography' => new Zend_Controller_Router_Route(
+                'discography',
+                array(
+                    'controller' => 'index',
+                    'action' => 'discography'
+                )
+            ),
+            'the-band' => new Zend_Controller_Router_Route(
+                'the-band',
+                array(
+                    'controller' => 'index',
+                    'action' => 'the-band'
+                )
+            ),
+            // Error Controller
+            'error' => new Zend_Controller_Router_Route(
+                'error',
+                    array(
+                        'controller' => 'error',
+                        'action' => 'template'
+                    )
+            ),
+        );
+        $router->addRoutes($routes);
+        return $router;
+    }
 }
 
