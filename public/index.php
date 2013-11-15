3bafc972e345cbbcac8dc2447d9b7e2ce8c6a6f6
Ricardo Buquet <ricardo.buquet@groupon.com>
2013-11-15 14:43:49 -0300

    cambio la estructura de carpetas para que sea soportada por el hosting

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
-
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
 
 // Define application environment
 defined('APPLICATION_ENV')
@@ -10,11 +9,11 @@ defined('APPLICATION_ENV')
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH . '/library'),
     get_include_path(),
 )));
 
-/** Zend_Application */
+/* Zend_Application */
 require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
@@ -22,5 +21,6 @@ $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
+
 $application->bootstrap()
             ->run();
